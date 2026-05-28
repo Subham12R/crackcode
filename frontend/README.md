10f4c0cb0ea3db79b62372035adb10c5d8b93398
subham12r <rikk4335@gmail.com>
2026-05-28 17:11:20 +0530

    v0.1: Frontend Structure created

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+Hey this is under development check this out soon! 
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+    <div className="bg-zinc-900 flex flex-col items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold text-white">CrackCode - Under Development</h1>
+    </div>
+    </>
+  );
+}
